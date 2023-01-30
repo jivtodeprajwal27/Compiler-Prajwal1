09162ca6073ec5bd5577784e07d9565f306d5bae
jivtodeprajwal27 <122209058+jivtodeprajwal27@users.noreply.github.com>
2023-01-30 12:21:55 +0530

    Add files via upload
    Added Testcases for Print operation and multiple number types Fraction and integer.

--- test_cases.py
+++ test_cases.py
@@ -1,5 +1,14 @@
 from start import *
 
+def test_typecheck():
+    import pytest
+    te = typecheck(BinOp("+", NumLiteral(2), NumLiteral(3)))
+    assert te.type == NumType
+    te = typecheck(BinOp("<", NumLiteral(2), NumLiteral(3)))
+    assert te.type == BoolType
+    # with pytest.raises(TypeError):
+    #     typecheck(BinOp("+", BinOp("*", NumLiteral(2), NumLiteral(3)), BinOp("<", NumLiteral(2), NumLiteral(3))))
+
 def test_div_operator():
     a  = Variable("a")
     n1= NumLiteral(5)
@@ -166,6 +175,119 @@ def test_less_than_equal_operator():
     e  = Let(c, n3, Let(b, n2,e2 ))
     assert eval(e) == True
 
+def test_unop():
+    
+    a=Variable("a")
+    e1=NumLiteral(1) 
+    e2=UnOp("++",e1)
+    e3=UnOp("-",e1)
+    e4=UnOp("--",e1)
+    assert eval(e3)== -1
+    assert eval(e2)==2
+    assert eval(e4)== 0
+
+    a=Variable("a")
+    e1=NumLiteral(2)
+    b=NumLiteral(1)
+    e2=BinOp("+",b,BinOp("*",UnOp("--",e1),e1))     #1+((2--)*2) = 3
+    assert eval(e2)==3
+    
+    
+    e1=NumLiteral(10)
+    b=NumLiteral(5)
+    c=NumLiteral(2)
+    e2=BinOp("+",UnOp("++",e1),BinOp("*",UnOp("-",c),b))     #(10++) + ((-2)*5) = 1
+    assert eval(e2)==1
+
+    a  = Variable("a")
+    e1 = NumLiteral(5)
+    e2 = BinOp("+", a, a)
+    e3 = UnOp("++",Let(a, e1, BinOp("+", a, Let(a, e2, e2))))
+    assert eval(e3)==26
+
+def test_bit():
+    # "Bitwise AND"
+    a=NumLiteral(15)
+    b=NumLiteral(4)
+    c=BinOp("&",a,b)   
+    assert eval(c)==4
+
+    # "Bitwise OR"
+    a=NumLiteral(12)
+    b=NumLiteral(20)
+    c=BinOp("|",a,b)
+    assert eval(c)==28
+
+    # "Bitwise XOR"
+    a=NumLiteral(34)
+    b=NumLiteral(41)
+    c=BinOp("^",a,b)
+    assert eval(c)==11
+
+def test_ls_rs():
+    
+    # "Bitwise Right Shift"
+    a=NumLiteral(10)
+    b=NumLiteral(1)
+    c=BinOp(">>",a,b)
+    assert eval(c)==5
+
+    # "Bitwise Left Shift"
+    a=NumLiteral(8)
+    b=NumLiteral(3)
+    c=BinOp("<<",a,b)
+    assert eval(c)==64
+
+def test_printop():
+    PrintOp(a>b)
+    a=1
+    b=PrintOp(a)
+    assert eval(b)==1
+    
+    a=3
+    b=2
+    c=PrintOp(a>b)
+    # d=eval(c)
+    # print(d)              //True
+    assert eval(c)== True
+
+    c=PrintOp(a<b)
+    # d=eval(c)
+    # print(d)             //False
+    assert eval(c)== False
+    
+    c=("This is an input string where a is ",a," b is ",b)
+    d=PrintOp(c)
+    assert eval(d) == ('This is an input string where a is ', 3, ' b is ', 2)
+    # e=eval(d)
+    # print(e)             //('This is an input string where a is ', 3, ' b is ', 2)
+
+    #PrintOp()              //print operation without input will give TypeError
+
+def test_division():
+    a=FracLiteral(2)               #using FracLiteral We will always get a fraction output
+    b=FracLiteral(4)
+    c=BinOp("/",a,b)
+    print(c)
+    assert eval(c)== 1/2
+
+    a=IntLiteral(9)                 #using IntLiteral We will always get an Integer output
+    b=IntLiteral(4)
+    c=BinOp("/",a,b)
+    print(c)
+    assert eval(c)== 2
+
+    a=NumLiteral(5)                 #Using Numliteral We can get float values
+    b=NumLiteral(2)
+    c=BinOp("/",a,b)
+    print(c)
+    assert eval(c)== 2.5
+
+    # a=IntLiteral(5)               #//If the opreands have different type Like Int and Fraction 
+    # b=FracLiteral(2)
+    # c=BinOp("/",a,b)
+    # print(c)
+    # assert eval(c)== 2.5
 
 test_div_operator()
 test_modulus_operator()
@@ -176,3 +298,8 @@ test_not_equal_operator()
 test_greter_than_operator()
 test_less_than_operator()
 test_less_than_equal_operator()
+test_unop()
+test_bit()
+test_ls_rs()
+test_printop()
+test_division()
